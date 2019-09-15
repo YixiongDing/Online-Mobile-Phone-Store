ddcd4d93b4ac8606342ded4544f072dd9a38bd2b
Yixiong <yixiongd@student.unimelb.edu.au>
2019-09-15 23:58:26 +1000

    Update Part 2

---
+++ OnlineMobilePhoneStoreManagementSystem/WebContent/ManageOrder.jsp
@@ -0,0 +1,77 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.util.*"%>
+<%@ page import="domain.Order"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Manage Mobile Phone</title>
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	crossorigin="anonymous">
+<link rel='stylesheet' href='style.css' />
+</head>
+<h1 class="h1-responsive font-weight-bold text-center my-5">Online
+	Mobile Phone Store Management System</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="">Welcome, Administrator</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse"
+		data-target="#navbarSupportedContent"
+		aria-controls="navbarSupportedContent" aria-expanded="false"
+		aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item"><a class="nav-link" href="LoginSuccess.jsp">Dashboard</a></li>
+			<li class="nav-item"><a class="nav-link"
+				href="ManageMobilePhoneServlet">Manage Mobile Phones</a></li>
+			<li class="nav-item active"><a class="nav-link"
+				href="ManageOrderServlet">Manage Orders <span class="sr-only">(current)</span>
+			</a></li>
+			<li class="nav-item"><a class="nav-link" href="#">Manage
+					Customer Accounts</a></li>
+		</ul>
+
+		<div class="nav-item  my-2 my-lg-0">
+			<a class="nav-link" href="HomePageServlet">Logout</a>
+		</div>
+	</div>
+</nav>
+<body>
+	<section class="text-center my-5">
+		<div class='container'>
+			<table class='table table-bordered table-striped'>
+				<tr>
+					<th>id</th>
+					<th>Create Time</th>
+					<th>Status</th>
+					<th>Mobile Id</th>
+					<th>Customer</th>
+					<th>Operation</th>
+				</tr>
+				<tbody>
+					<%
+						List<Order> orders = (ArrayList<Order>) request.getAttribute("orders");
+
+						for (Order order : orders) {
+							out.println("<form action=\"ManageOrderServlet\" method=\"post\">");
+							out.print("<tr><td>" + order.getId() + "</td><td>" + order.getCreateTime() + "</td><td>"
+									+ order.getStatus() + "</td><td>" + order.getMobilePhone().getId() + "</td><td>"
+									+ order.getCustomer().getCustomerName() + "</td>" + "<td colspan=\"2\" align=\"center\">"
+									+ "<button style=\"margin: 0px 10px 0px 10px;\" type=\"submit\" name=\"update\" value=\""
+									+ order.getId() + "\" class=\"btn btn-primary\">Update</button>"
+									+ "<button style=\"margin: 0px 10px 0px 10px;\" type=\"submit\" name=\"delete\" value=\""
+									+ order.getId() + "\" class=\"btn btn-primary\">Cancel</button></td></tr>");
+							out.println("</form>");
+						}
+					%>
+				</tbody>
+			</table>
+		</div>
+	</section>
+</body>
+</html>
---
+++ OnlineMobilePhoneStoreManagementSystem/src/dataMapper/MobilePhoneMapper.java
@@ -0,0 +1,201 @@
+package dataMapper;
+import domain.MobilePhone;
+import domain.Model;
+import domain.DomainObject;
+import java.util.ArrayList;
+import java.util.List;
+
+import database.DBConnection;
+
+import java.sql.*;
+
+public class MobilePhoneMapper extends DataMapper{
+
+	private final static String findMobilePhoneStatement =
+			"SELECT * " +
+					"  FROM mobilephone " +
+					"  WHERE mobilephoneid = ? ";
+
+	private static final String findAvaliableMobilePhoneStatement =
+			"SELECT * " +
+					" FROM mobilephone " +
+					" WHERE qty > 0 ";
+
+	private static final String findAllMobilePhoneStatement =
+			"SELECT * " + 
+					" FROM mobilephone ";
+	
+	private static final String updateMobilePhoneStatement =
+			"UPDATE mobilephone "+
+					" SET modelname = ?, storagesize=?, color=?, brand = ?, price = ?, qty = ?, description = ? " +
+					" WHERE mobilephoneid  = ? ";
+	
+	private static final String insertMobilePhoneStatement = 
+			"INSERT INTO mobilephone " +
+					" (mobilephoneid, modelname, storagesize,color, brand, price, qty, description) "+
+					" VALUES (?, ?, ?, ?, ?, ?, ?, ?); ";
+	
+	private static final String deleteMobilePhoneStatement = 
+				"DELETE " +
+					" FROM mobilephone " +
+					" WHERE mobilephoneid  = ? ";
+	
+
+	public List<MobilePhone> findMobilePhone(MobilePhone mobile) {
+
+		List<MobilePhone> result = new ArrayList<MobilePhone>();
+		try {
+			Connection dbConnection = DBConnection.getDBConnection();
+			PreparedStatement findStatement = DBConnection.prepare(findMobilePhoneStatement, dbConnection);
+			findStatement.setInt(1, mobile.getMobilePhoneId());
+			ResultSet rs = findStatement.executeQuery();
+
+			while(rs.next()) {
+				MobilePhone m = new MobilePhone();
+				IdentityMap<MobilePhone> identityMap = IdentityMap.getInstance(m);
+				m.setMobilePhoneId(rs.getInt(1));
+				m.setModelName(rs.getString(2));
+				m.setStorageSize(rs.getString(3));
+				m.setColor(rs.getString(4));
+				m.setBrand(rs.getString(5));
+				m.setPrice(rs.getFloat(6));
+				m.setQty(rs.getInt(7));
+				m.setDescription(rs.getString(8));
+				identityMap.put(m.getMobilePhoneId(),m);
+				result.add(m);
+			}
+
+		} catch (SQLException e) {
+		}
+		return result;
+	}
+
+	public List<MobilePhone> findAllMobilePhone(){
+		List<MobilePhone> result = new ArrayList<MobilePhone>();
+		try {
+			Connection dbConnection = DBConnection.getDBConnection();
+			PreparedStatement findStatement = DBConnection.prepare(findAllMobilePhoneStatement, dbConnection);
+			ResultSet rs = findStatement.executeQuery();
+
+			while(rs.next()) {
+				MobilePhone m = new MobilePhone();
+				IdentityMap<MobilePhone> identityMap = IdentityMap.getInstance(m);
+				m.setMobilePhoneId(rs.getInt(1));
+				m.setModelName(rs.getString(2));
+				m.setStorageSize(rs.getString(3));
+				m.setColor(rs.getString(4));
+				m.setBrand(rs.getString(5));
+				m.setPrice(rs.getFloat(6));
+				m.setQty(rs.getInt(7));
+				m.setDescription(rs.getString(8));
+				identityMap.put(m.getMobilePhoneId(),m);
+				result.add(m);;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public List<MobilePhone> findAvailableMobilePhone() {
+		List<MobilePhone> result = new ArrayList<>();
+		try {
+			Connection dbConnection = DBConnection.getDBConnection();
+			PreparedStatement findStatement = DBConnection.prepare(findAvaliableMobilePhoneStatement, dbConnection);
+			ResultSet rs = findStatement.executeQuery();
+			while (rs.next()) {
+				MobilePhone m = new MobilePhone();
+				IdentityMap<MobilePhone> identityMap = IdentityMap.getInstance(m);
+				m.setMobilePhoneId(rs.getInt(1));
+				m.setModelName(rs.getString(2));
+				m.setStorageSize(rs.getString(3));
+				m.setColor(rs.getString(4));
+				m.setBrand(rs.getString(5));
+				m.setPrice(rs.getFloat(6));
+				m.setQty(rs.getInt(7));
+				m.setDescription(rs.getString(8));
+				identityMap.put(m.getMobilePhoneId(),m);
+				result.add(m);;
+			}
+
+		} catch (SQLException e) {
+
+		}
+		return result;
+	}
+
+	@Override
+	public boolean update(DomainObject obj) {
+		MobilePhone mobile = (MobilePhone) obj;
+
+		int result = 0;
+		try {
+			Connection dbConnection = DBConnection.getDBConnection();
+			PreparedStatement findStatement = DBConnection.prepare(updateMobilePhoneStatement, dbConnection);				
+			findStatement.setString(1, mobile.getModelName());
+			findStatement.setString(2, mobile.getStorageSize());
+			findStatement.setString(3, mobile.getColor());
+			findStatement.setString(4, mobile.getBrand());
+			findStatement.setFloat(5, mobile.getPrice());
+			findStatement.setInt(6, mobile.getQty());
+			findStatement.setString(7, mobile.getDescription());
+			findStatement.setInt(8, mobile.getMobilePhoneId());
+			result = findStatement.executeUpdate();
+			DBConnection.closePreparedStatement(findStatement);
+			DBConnection.closeConnection(dbConnection);		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		if (result == 0)
+			return false;
+		else 
+			return true;
+	}
+	
+	@Override
+	public boolean insert(DomainObject obj) {
+		MobilePhone mobile = (MobilePhone) obj;
+		int result = 0;
+		try {
+			Connection dbConnection = DBConnection.getDBConnection();
+			PreparedStatement findStatement = DBConnection.prepare(insertMobilePhoneStatement, dbConnection);
+			findStatement.setInt(1, mobile.getMobilePhoneId());
+			findStatement.setString(2, mobile.getModelName());
+			findStatement.setString(3, mobile.getStorageSize());
+			findStatement.setString(4, mobile.getColor());
+			findStatement.setString(5, mobile.getBrand());
+			findStatement.setFloat(6, mobile.getPrice());
+			findStatement.setInt(7, mobile.getQty());
+			findStatement.setString(8, mobile.getDescription());
+			result = findStatement.executeUpdate();
+			DBConnection.closePreparedStatement(findStatement);
+			DBConnection.closeConnection(dbConnection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (result == 0)
+			return false;
+		else 
+			return true;
+	}
+
+	@Override
+	public boolean delete(DomainObject obj) {
+		MobilePhone mobile = (MobilePhone) obj;
+		int result = 0;
+		try {
+			Connection dbConnection = DBConnection.getDBConnection();
+			PreparedStatement findStatement = DBConnection.prepare(deleteMobilePhoneStatement, dbConnection);
+			findStatement.setInt(1, mobile.getMobilePhoneId());
+			result = findStatement.executeUpdate();
+			DBConnection.closePreparedStatement(findStatement);
+			DBConnection.closeConnection(dbConnection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (result == 0)
+			return false;
+		else 
+			return true;
+	}
+
+}
